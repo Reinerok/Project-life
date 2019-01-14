21a798eb4ce2a891337ef0cb1f7812c74bb1fb9f
Reinerok <reinerok@gmail.com>
2019-01-15 00:26:45 +0500

    update 2
    update 2

--- js/game.js
+++ js/game.js
@@ -4,16 +4,25 @@ const GRID_ROWS = 36;
 const GRID_COLS = 64;
 const GAME_SPEED = 100;
 
-const grid = createGrid(GRID_ROWS,GRID_COLS);
-const nextGrid = createGrid(GRID_ROWS,GRID_COLS);
+const grid = createGrid(GRID_ROWS, GRID_COLS);
+const nextGrid = createGrid(GRID_ROWS, GRID_COLS);
 
 let isPlaying = false;
+
+let interval = 0;
 
 const root = document.getElementById('root');
 
 const table = createTable(GRID_ROWS, GRID_COLS);
-
 createControls();
+
+function play() {
+    computeNextGrid();
+    updateView();
+    if (isPlaying) {
+    setTimeout(play,500);
+    }
+}
 
 function updateView() {
     for (let i = 0; i < grid.length; i++) {
@@ -21,10 +30,7 @@ function updateView() {
             const cell = table.rows[i].cells[j];
             const isCellAlive = grid[i][j];
 
-            console.log(isCellAlive);
-            
-            
-            cell.classList.toggle('alive',isCellAlive)
+            cell.classList.toggle('alive', isCellAlive);
         }
     }
 }
@@ -35,25 +41,25 @@ function createTable(rows, cols) {
     table.className = 'grid';
 
     for (let i = 0; i < rows; i++) {
-        const row  = document.createElement('tr');
-        
+        const row = document.createElement('tr');
+
         row.className = 'row';
 
         for (let j = 0; j < cols; j++) {
-            const cell  = document.createElement('td');
-            
+            const cell = document.createElement('td');
+
             cell.className = 'cell';
             cell.width = GRID_WIDTH / cols;
             cell.height = GRID_HEIGHT / rows;
 
-            row.appendChild(cell)   
+            row.appendChild(cell);
         }
 
         table.appendChild(row);
     }
 
     table.addEventListener('click', event => {
-        
+
         if (!event.target.classList.contains('cell')) return;
 
         const cell = event.target;
@@ -63,11 +69,7 @@ function createTable(rows, cols) {
 
         grid[rowIndex][celIndex] = isCellAlive ? 0 : 1;
 
-
         cell.classList.toggle('alive', !isCellAlive);
-
-        console.log(grid);
-        
     });
 
     root.appendChild(table);
@@ -86,8 +88,9 @@ function createControls() {
         } else {
             isPlaying = true;
             this.textContent = 'pause';
+            play();
         }
-    })
+    });
 
     const restButton = document.createElement('button');
     restButton.className = 'material-icons';
@@ -95,7 +98,9 @@ function createControls() {
     restButton.addEventListener('click', function () {
         isPlaying = false;
         startButton.textContent = 'play_arrow';
-    })
+        restartGrid();
+        updateView();
+    });
 
     const randomizeButton = document.createElement('button');
     randomizeButton.className = 'material-icons';
@@ -105,7 +110,7 @@ function createControls() {
         startButton.textContent = 'play_arrow';
 
         randomizeGrid();
-        updateView()        
+        updateView();
     });
 
     const container = document.createElement('div');
@@ -117,13 +122,12 @@ function createControls() {
 }
 
 function createGrid(rows, cols) {
-    
-    
+
     const grid = [];
 
     for (let i = 0; i < rows; i++) {
         grid[i] = [];
-        
+
         for (let j = 0; j < cols; j++) {
             grid[i][j] = 0;
         }
@@ -138,4 +142,89 @@ function randomizeGrid() {
             grid[i][j] = Math.round(Math.random());
         }
     }
+}
+
+function restartGrid() {
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            grid[i][j] = 0;
+        }
+    }
+}
+
+function computeNextGrid() {
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            applyRules(i, j);
+        }
+    }
+
+    copyNextGrid();
+}
+
+function copyNextGrid() {
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            grid[i][j] = nextGrid[i][j];
+            nextGrid[i][j] = 0;
+        }
+    }
+}
+
+function applyRules(row, col) {
+    const isCellAlive = grid[row][col];
+    const numberOfNeighbors = countNeighbors(row, col);
+
+     if (isCellAlive) {
+         if (numberOfNeighbors < 2) {
+            nextGrid[row][col] = 0;
+         } else if (numberOfNeighbors === 2 || numberOfNeighbors === 3) {
+            nextGrid[row][col] = 1;
+         } else if (numberOfNeighbors > 3) {
+            nextGrid[row][col] = 0;
+         }
+     } else {
+         if (numberOfNeighbors === 3) {
+            nextGrid[row][col] = 1;
+        }
+     }
+}
+
+function countNeighbors(row, col) {
+    let count = 0;
+
+    if(row - 1 >= 0) { // top
+        if (grid[row - 1][col] === 1) count++;
+    }
+
+    if (row - 1 >= 0 && col - 1 >= 0) { // top left
+        if (grid[row - 1][col - 1] === 1) count++;
+    }
+
+    if (row - 1 >= 0 && col + 1 < GRID_COLS) { // top right
+        if (grid[row - 1][col + 1] === 1) count++;
+    }
+
+    if (col - 1 >= 0) { // left
+        if (grid[row][col - 1] === 1) count++;
+    }
+
+    if (col + 1 < GRID_COLS) { // right
+        if (grid[row][col + 1] === 1) count++;
+    }
+
+    if (row + 1 < GRID_ROWS) { // bottom
+        if (grid[row + 1][col] === 1) count++;
+    }
+
+    if (row + 1 < GRID_ROWS && col - 1 >= 0) { // bottom left
+        if (grid[row + 1][col - 1] === 1) count++;
+    }
+
+    if (row + 1 < GRID_ROWS && col + 1 < GRID_COLS) { // bottom right
+        if (grid[row + 1][col + 1] === 1) count++;
+    }
+
+    return count;
+
 }
